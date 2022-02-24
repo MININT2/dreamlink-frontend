d5c1ce3692dbb4c6f1de8d75cd41907feb302979
MININT2 <minint.dsk@gmail.com>
2022-02-23 18:36:42 -0600

    funnies

---
+++ dreamlink-gui/src/components/page-navbar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+// string array should only contain button names
+defineProps<{ content: string[] }>()
+data: {
+  content:[
+    { enum: '�'},
+    { enum: ''},
+    { enum: ''}
+  ]
+}
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="txt in content">
+        {{ content.enum }}
+      </li>
+    </ul>
+  </div>
+  <button type="button" @click="">{{ "text" }}</button>
+</template>
+
+<style scoped>
+code {
+  background-color: inherit;
+  color: $;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ dreamlink-gui/src/router.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("./views/Home.vue"),
+    meta: {
+      title: "minint",
+    },
+  },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
